b03104ad60403060253934f87a42aecbd061633b
SukriTM <micsu.ky@gmail.com>
2023-03-30 16:03:13 +0700

    Lab23 completed

--- average.cpp
+++ average.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+#include<cstdlib>
 
+using namespace std;
+int main(int argc,char *argv[]){
+    
+    float x = 0.0;
+    if(argc == 1){
+        cout << "Please input numbers to find average." << endl;
+    }else {
+    cout << "---------------------------------" <<endl;
+    cout << "Average of " << argc-1 << " numbers = ";
+    for(int i=1; i<argc; i++){
+        x += atof(argv[i])  ;
+    }
+    cout << x/(argc-1) << endl;
+    cout <<"---------------------------------" <<endl;
+    }
+    
+}
